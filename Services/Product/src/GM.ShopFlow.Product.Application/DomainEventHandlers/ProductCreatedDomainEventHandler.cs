9c3af8738b5c35ab57e3a48ba4381e9e609f8f45
Guilherme2907 <guilherme.magalhaes2907@gmail.com>
2025-04-25 23:51:56 -0300

    Creation of domain event handlers of product service

---
+++ Services/Product/src/GM.ShopFlow.Product.Application/DomainEventHandlers/ProductCreatedDomainEventHandler.cs
@@ -0,0 +1,12 @@
+﻿using GM.ShopFlow.Product.Domain.Events;
+using MediatR;
+
+namespace GM.ShopFlow.Product.Application.DomainEventHandlers;
+
+public class ProductCreatedDomainEventHandler : INotificationHandler<ProductCreatedDomainEvent>
+{
+    public Task Handle(ProductCreatedDomainEvent notification, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Services/Product/src/GM.ShopFlow.Product.Application/DomainEventHandlers/StockUpdatedDomainEventHandler.cs
@@ -0,0 +1,12 @@
+﻿using GM.ShopFlow.Product.Domain.Events;
+using MediatR;
+
+namespace GM.ShopFlow.Product.Application.DomainEventHandlers;
+
+public class StockUpdatedDomainEventHandler : INotificationHandler<StockUpdatedDomainEvent>
+{
+    public Task Handle(StockUpdatedDomainEvent notification, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
