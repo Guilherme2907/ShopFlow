75c6178300f1c9b6fc275b28090ae2212728fcb1
Guilherme2907 <guilherme.magalhaes2907@gmail.com>
2025-04-25 23:39:00 -0300

    Creation of domain event handlers of order service

---
+++ Services/Order/src/GM.ShopFlow.Order.Application/DomainEventHandlers/CustomerCreatedDomainEventhandler.cs
@@ -0,0 +1,12 @@
+﻿using GM.ShopFlow.Order.Domain.Events;
+using MediatR;
+
+namespace GM.ShopFlow.Order.Application.DomainEventHandlers;
+
+public class CustomerCreatedDomainEventhandler : INotificationHandler<CustomerCreatedDomainEvent>
+{
+    public Task Handle(CustomerCreatedDomainEvent notification, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Services/Order/src/GM.ShopFlow.Order.Application/DomainEventHandlers/OrderCreatedDomainEventHandler.cs
@@ -0,0 +1,12 @@
+﻿using GM.ShopFlow.Order.Domain.Events;
+using MediatR;
+
+namespace GM.ShopFlow.Order.Application.DomainEventHandlers;
+
+public class OrderCreatedDomainEventHandler : INotificationHandler<OrderCreatedDomainEvent>
+{
+    public Task Handle(OrderCreatedDomainEvent notification, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
