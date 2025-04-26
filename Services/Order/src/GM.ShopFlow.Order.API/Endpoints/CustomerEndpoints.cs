5bdc7e3623dc85d272d332366e008cc23e7efba3
Guilherme2907 <guilherme.magalhaes2907@gmail.com>
2025-04-25 23:38:25 -0300

    Creation of customer and order endpoints

---
+++ Services/Order/src/GM.ShopFlow.Order.API/Endpoints/CustomerEndpoints.cs
@@ -0,0 +1,33 @@
+﻿using GM.ShopFlow.Order.Application.UseCases.Customer.CreateCustomer;
+using MediatR;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading;
+
+namespace GM.ShopFlow.Order.API.Endpoints;
+
+public static class CustomerEndpoints
+{
+    public static IEndpointRouteBuilder MapCustomerEndpoints(this IEndpointRouteBuilder builder)
+    {
+        var group = builder
+            .MapGroup("api/customers")
+            .WithTags("Customers")
+            .WithOpenApi();
+
+        group.MapPost("/", CreateCustomer);
+
+        return builder;
+    }
+
+    public static async Task<Ok> CreateCustomer(
+        [FromBody] CreateCustomerInput input,
+        [FromServices] IMediator mediator,
+        CancellationToken cancellationToken
+    )
+    {
+        await mediator.Send(input, cancellationToken);
+
+        return TypedResults.Ok();
+    }
+}
---
+++ Services/Order/src/GM.ShopFlow.Order.API/Endpoints/OrderEndpoints.cs
@@ -0,0 +1,37 @@
+﻿using GM.ShopFlow.Order.Application.UseCases.Order.CreateOrder;
+using GM.ShopFlow.Order.Application.UseCases.Order.GetOrders;
+using MediatR;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GM.ShopFlow.Order.API.Endpoints;
+
+public static class OrderEndpoints
+{
+    public static IEndpointRouteBuilder MapOrderEndpoints(this IEndpointRouteBuilder builder)
+    {
+        var group = builder
+            .MapGroup("api/orders")
+            .WithTags("Orders")
+            .WithOpenApi();
+
+        group.MapPost("/", CreateOrder);
+        group.MapGet("/{customerId}", GetOrders);
+
+        return builder;
+    }
+
+    public static async Task<Ok> CreateOrder([FromBody] CreateOrderInput input, [FromServices] IMediator mediator)
+    {
+        await mediator.Send(input, CancellationToken.None);
+
+        return TypedResults.Ok();
+    } 
+    
+    public static async Task<Ok<IEnumerable<GetOrdersOutput>>> GetOrders(Guid customerId, [FromServices] IMediator mediator)
+    {
+        var result = await mediator.Send(new GetOrdersInput(customerId), CancellationToken.None);
+
+        return TypedResults.Ok(result);
+    }
+}
