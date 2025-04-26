5288f98b6092fd6ac37f41e1eed51f9adfbdb53a
Guilherme2907 <guilherme.magalhaes2907@gmail.com>
2025-04-25 23:52:22 -0300

    Creation of integration events of product service

---
+++ Services/Product/src/GM.ShopFlow.Product.Application/IntegrationsEvents/Events/OrderCreatedIntegrationEvent.cs
@@ -0,0 +1,7 @@
+﻿using EventBus.Events;
+
+namespace GM.ShopFlow.Product.Application.IntegrationsEvents.Events;
+
+public class OrderCreatedIntegrationEvent : IntegrationEvent
+{
+}
---
+++ Services/Product/src/GM.ShopFlow.Product.Application/IntegrationsEvents/Events/StockUpdatedIntegrationEvent.cs
@@ -0,0 +1,7 @@
+﻿using EventBus.Events;
+
+namespace GM.ShopFlow.Product.Application.IntegrationsEvents.Events;
+
+public class StockUpdatedIntegrationEvent : IntegrationEvent
+{
+}
