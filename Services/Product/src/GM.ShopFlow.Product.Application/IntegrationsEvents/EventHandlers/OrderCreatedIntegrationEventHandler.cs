e20d7a54754d382a25c5228f9a827d0196e8e15a
Guilherme2907 <guilherme.magalhaes2907@gmail.com>
2025-04-25 23:51:47 -0300

    Creation of integration event handlers of product service

---
+++ Services/Product/src/GM.ShopFlow.Product.Application/IntegrationsEvents/EventHandlers/OrderCreatedIntegrationEventHandler.cs
@@ -0,0 +1,13 @@
+﻿using EventBus.Abstractions;
+using GM.ShopFlow.Product.Application.IntegrationsEvents.Events;
+
+namespace GM.ShopFlow.Product.Application.IntegrationsEvents.EventHandlers;
+
+public class OrderCreatedIntegrationEventHandler
+    : IIntegrationEventHandler<OrderCreatedIntegrationEvent>
+{
+    public Task HandleAsync(OrderCreatedIntegrationEvent @event, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Services/Product/src/GM.ShopFlow.Product.Application/IntegrationsEvents/EventHandlers/StockUpdatedIntegrationEventHandler.cs
@@ -0,0 +1,13 @@
+﻿using EventBus.Abstractions;
+using GM.ShopFlow.Product.Application.IntegrationsEvents.Events;
+
+namespace GM.ShopFlow.Product.Application.IntegrationsEvents.EventHandlers;
+
+public class StockUpdatedIntegrationEventHandler
+    : IIntegrationEventHandler<StockUpdatedIntegrationEvent>
+{
+    public Task HandleAsync(StockUpdatedIntegrationEvent @event, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
