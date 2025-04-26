a3cddfb3a32672c30598dc472211befb01fb4690
Guilherme2907 <guilherme.magalhaes2907@gmail.com>
2025-04-25 23:47:12 -0300

    Registration of services DI

--- Services/Order/src/GM.ShopFlow.Order.API/Program.cs
+++ Services/Order/src/GM.ShopFlow.Order.API/Program.cs
@@ -1,4 +1,12 @@
+using GM.ShopFlow.Order.API.Endpoints;
+using GM.ShopFlow.Order.Application.Interfaces;
+using GM.ShopFlow.Order.Application.UseCases.Customer.CreateCustomer;
+using GM.ShopFlow.Order.Application.UseCases.Order.CreateOrder;
+using GM.ShopFlow.Order.Application.UseCases.Order.GetOrders;
+using GM.ShopFlow.Order.Domain.SeedWork.Repositories;
+using GM.ShopFlow.Order.Infra.Data;
 using GM.ShopFlow.Order.Infra.Data.Context;
+using GM.ShopFlow.Order.Infra.Data.Repositories;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
@@ -36,6 +44,15 @@ builder.Services.AddDbContext<OrderDbContext>(options =>
     options.UseSqlServer(builder.Configuration["ConnectionStrings:DefaultConnection"])
 );
 
+builder.Services.AddMediatR(config => config.RegisterServicesFromAssemblies(typeof(CreateOrder).Assembly));
+builder.Services.AddScoped<IOrderRepository, OrderRepository>();
+builder.Services.AddScoped<ICustomerRepository, CustomerRepository>();
+builder.Services.AddScoped<ICreateOrder, CreateOrder>();
+builder.Services.AddScoped<IGetOrders, GetOrders>();
+builder.Services.AddScoped<ICreateCustomer, CreateCustomer>();
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -49,6 +66,9 @@ app.UseHttpsRedirection();
 
 app.UseAuthentication();
 app.UseAuthorization();
+
+app.MapOrderEndpoints()
+    .MapCustomerEndpoints();
 
 app.Run();
 
